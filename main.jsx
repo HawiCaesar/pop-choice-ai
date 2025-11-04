68947a156306226e3c0e90969e62a253fcaedaa4
HawiCaesar <brianhawi92@gmail.com>
2025-11-04 19:12:04 +0300

    feat(): add more logs

--- main.jsx
+++ main.jsx
@@ -66,7 +66,7 @@ export const App = () => {
       );
       const data = await response.json();
 
-      console.log(JSON.parse(data.content).movieRecommendations)
+      console.log(JSON.parse(data.content))
 
       setShowAiRecommendations(true);
       setAiRecommendations(JSON.parse(data.content).movieRecommendations);
